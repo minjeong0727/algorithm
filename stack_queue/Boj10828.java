a08c966eb3ce99e6ed2dd2ae185e2cd193c951b1
minjeong0727 <gominjeong@MacBook-Pro-6.local>
2024-07-08 16:30:34 +0900

    스택 이론 & Boj10828

--- stack_queue/Boj10828.java
+++ stack_queue/Boj10828.java
@@ -1,4 +1,34 @@
 package stack_queue;
 
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Boj10828 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        while(N --> 0){
+            String[]cmd = br.readLine().split(" ");
+            if(cmd[0].equals("push")){
+                stack.addLast(Integer.parseInt(cmd[1]));
+            }
+            else if(cmd[0].equals("pop")){
+                bw.append(stack.isEmpty()?"-1\n": stack.removeLast()+"\n");
+            }else if(cmd[0].equals("size")){
+                bw.append(String.valueOf(stack.size())).append("\n");
+            }else if(cmd[0].equals("empty")){
+                bw.append(stack.isEmpty()?"1\n":"0\n");
+            }else if(cmd[0].equals("top")){
+                bw.append(stack.isEmpty()?"-1\n": stack.getLast()+"\n");
+            }
+        }
+        bw.flush();
+    }
+
 }
---
+++ stack_queue/Boj9012.java
@@ -0,0 +1,10 @@
+package stack_queue;
+
+import java.util.Deque;
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class Boj9012 {
+
+
+}
