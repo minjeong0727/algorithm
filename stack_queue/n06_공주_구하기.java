3b83565ecd073f7f3a2fede7ace5b8adbf5660a1
SunMi <wywudi@naver.com>
2024-07-10 19:59:20 +0900

    Queue

--- stack_queue/n06_공주_구하기.java
+++ stack_queue/n06_공주_구하기.java
@@ -7,17 +7,14 @@ import java.util.Scanner;
 public class n06_공주_구하기 {
     public int solution(int n, int k) {
         Queue<Integer> q = new LinkedList<Integer>();
-        int p = 0;
-        //queue에 n까지의 자연수를 추가
+
         for (int i = 1; i <= n; i++) q.offer(i);
-        //queue의 size가 1일 때까지
+
         while (q.size() != 1) {
-            int t=q.poll();
-            p++;
-            if (p == k) {
-                p = 0;
-            }else q.offer(t);
-        }
+            for (int i = 1; i < k; i++) {
+                q.offer(q.poll());}
+            q.poll();
+            }
         return q.poll();
     }
 
