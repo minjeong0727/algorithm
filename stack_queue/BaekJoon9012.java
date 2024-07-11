74f6209dde0a1fe83247bae99e643fc65f32f5d7
dawonseo <oloielo@naver.com>
2024-07-11 16:39:13 +0900

    update class name

--- stack_queue/BaekJoon9012.java
+++ stack_queue/BaekJoon9012.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class BaekJoon9012 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
